3c444b83de9e3069ee095841af074644dc8318c7
Oleksandr Kovshyk <oleksandrkovshyk95@gmail.com>
2022-01-21 16:07:54 +0200

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(number) {
+    number = Math.abs(number);
+    numberToString = number.toString();
+    result = numberToString.split("").reverse().join("");
+    stringToNumber = Number(result);
+    return stringToNumber;
+};
